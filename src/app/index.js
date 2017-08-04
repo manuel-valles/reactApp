d37398cdf14e075afbb5379e4c642cee7c22d6a3
manukempo <manukempo@gmail.com>
2017-08-04 15:59:12 +0100

    First Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
-alert('It works!!');
+// Import react and react-dom
+var React = require('react');
+var ReactDOM =  require('react-dom');
+
+// Create component
+var TodoComponent = React.createClass({
+	render: function(){
+		return(
+			<div>
+			<h1>Hello World!!</h1>
+			<p>More than one line</p>
+			</div>
+		);
+	}
+});
+
+// Put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
