9caf2e8fd1a9e672ef205ad067f997a93cb2952f
manukempo <manukempo@gmail.com>
2017-08-04 17:54:14 +0100

    State

--- src/app/index.js
+++ src/app/index.js
@@ -4,16 +4,29 @@ var ReactDOM =  require('react-dom');
 
 // Create component
 var TodoComponent = React.createClass({
+	getInitialState: function(){
+		return {
+			todos:['wash up', 'eat some cheese', 'take a nap']
+		}
+	},
 	render: function(){
 		return(
 			<div>
-			<h1>Hello World!!</h1>
-			<p>More than one line.</p>
-			<p>{this.props.mssg}</p>
-			<p>If that contains cheese, wow!!</p>
-			<p><strong>Cheese name: </strong>{this.props.cheese.name}</p>
-			<p><strong>Cheese smell factor: </strong>{this.props.cheese.smellFactor}</p>
-			<p><strong>Cheese price: </strong>{this.props.cheese.price}</p>
+				<h1>Hello World!!</h1>
+				<p>More than one line.</p>
+				<p>{this.props.mssg}</p>
+				<p>If that contains cheese, wow!!</p>
+				<p><strong>Cheese name: </strong>{this.props.cheese.name}</p>
+				<p><strong>Cheese smell factor: </strong>{this.props.cheese.smellFactor}</p>
+				<p><strong>Cheese price: </strong>{this.props.cheese.price}</p>
+				<div id="todo-list">
+					<p>The busiest people have the most leisure...</p>
+					<ul>
+						<li>{this.state.todos[0]}</li>
+						<li>{this.state.todos[1]}</li>
+						<li>{this.state.todos[2]}</li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
