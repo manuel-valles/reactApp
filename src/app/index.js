d893f7992f3ff771b7c40b35bbba921569f4d2df
manukempo <manukempo@gmail.com>
2017-08-04 16:27:14 +0100

    Props

--- src/app/index.js
+++ src/app/index.js
@@ -8,11 +8,17 @@ var TodoComponent = React.createClass({
 		return(
 			<div>
 			<h1>Hello World!!</h1>
-			<p>More than one line</p>
+			<p>More than one line.</p>
+			<p>{this.props.mssg}</p>
+			<p>If that contains cheese, wow!!</p>
+			<p><strong>Cheese name: </strong>{this.props.cheese.name}</p>
+			<p><strong>Cheese smell factor: </strong>{this.props.cheese.smellFactor}</p>
+			<p><strong>Cheese price: </strong>{this.props.cheese.price}</p>
 			</div>
 		);
 	}
 });
 
+var myCheese = {name:'Camembert', smellFactor: 'Extreme pong', price:'3.50'};
 // Put component into html page
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent mssg="I love pizza." cheese={myCheese}/>, document.getElementById('todo-wrapper'));
