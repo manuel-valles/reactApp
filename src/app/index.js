29d45206acce9212f1d00a48ec7fad9bee57bd43
manukempo <manukempo@gmail.com>
2017-08-05 17:19:16 +0100

    Routing - Deprecated React-Router

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,43 @@
 // Import react and react-dom
 var React = require('react');
 var ReactDOM =  require('react-dom');
+// Import Router and Route with ES6
+
+// OPTION A: IMPORTING THE DEPRECATED REACT-ROUTER
+// import { Router, Route, browserHistory } from 'react-router';
+
+// OPTION B: IMPORTING THE REACT-ROUTER-DOM
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 // Module requires
 var TodoItem = require('./todoItem');
 var AddItem = require('./addItem');
+var About = require('./about');
 
-//CSS requires
+// CSS requires
 require('./css/index.css');
+
+// Set the Route
+var App = React.createClass({
+	render: function(){
+		// OPTION A: WITH DEPRECATED REACT-ROUTER
+		/*return(
+            <Router history={browserHistory}>
+                <Route path={"/"} component={TodoComponent}></Route>
+                <Route path={"/about"} component={About}></Route>
+            </Router>
+        );*/
+        // OPTION B: UP TO DATE
+        return(
+            <Router>
+                    <Switch>
+                      <Route exact={true} path={'/'} component={TodoComponent} />
+                      <Route path={'/about'} component={About} />
+                    </Switch>
+            </Router>
+        );
+	}
+});
 
 // Create component
 var TodoComponent = React.createClass({
@@ -31,12 +61,14 @@ var TodoComponent = React.createClass({
 				<TodoItem item={item} key={index} onDelete={this.onDelete} />
 			);
 		}.bind(this));
-		return(
+		// BackUp
+		/*return(
 			<div>
 				<h1>Hello World!!</h1>
 				<p>More than one line.</p>
 				<p>{this.props.mssg}</p>
 				<p>If that contains cheese, wow!!</p>
+
 				<p><strong>Cheese name: </strong>{this.props.cheese.name}</p>
 				<p><strong>Cheese smell factor: </strong>{this.props.cheese.smellFactor}</p>
 				<p><strong>Cheese price: </strong>{this.props.cheese.price}</p>
@@ -54,7 +86,16 @@ var TodoComponent = React.createClass({
 					<AddItem onAdd={this.onAdd}/>
 				</div>
 			</div>
-		); 
+		); */
+
+		return(
+            <div id="todo-list">
+                <p>The busiest people have the most leisure...</p>
+                <ul>{todos}</ul>
+                <AddItem onAdd={this.onAdd} />
+            </div>
+        );
+
 	}, // render
 
 	// Custom functions
@@ -77,10 +118,22 @@ var TodoComponent = React.createClass({
 		this.setState({
 			todos: updatedTodos
 		});
-	}
+	},
 
+	//lifecycle functions
+	componentWillMount: function(){
+		console.log('componentWillMount');
+	},
+	componentDidMount: function(){
+		console.log('componentDidMount');
+		// any grabbing of external data.
+	},
+	componentWillUpdate: function(){
+		console.log('componentWillUpdate');
+	}
 });
 
 var myCheese = {name:'Camembert', smellFactor: 'Extreme pong', price:'3.50'};
 // Put component into html page
-ReactDOM.render(<TodoComponent mssg="I love pizza." cheese={myCheese}/>, document.getElementById('todo-wrapper'));
+// ReactDOM.render(<TodoComponent mssg="I love pizza." cheese={myCheese}/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<App />, document.getElementById('todo-wrapper'));
