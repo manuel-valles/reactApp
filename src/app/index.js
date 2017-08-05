0f43554757130ea140d167265482db23ec981b57
manukempo <manukempo@gmail.com>
2017-08-05 10:54:15 +0100

    Handling Events

--- src/app/index.js
+++ src/app/index.js
@@ -21,9 +21,9 @@ var TodoComponent = React.createClass({
 		todos = todos.map(function(item, index){
 			return(
 				// <li>{item +"!"}</li>
-				<TodoItem item={item} key={index} />
+				<TodoItem item={item} key={index} onDelete={this.onDelete} />
 			);
-		});
+		}.bind(this));
 		return(
 			<div>
 				<h1>Hello World!!</h1>
@@ -34,7 +34,7 @@ var TodoComponent = React.createClass({
 				<p><strong>Cheese smell factor: </strong>{this.props.cheese.smellFactor}</p>
 				<p><strong>Cheese price: </strong>{this.props.cheese.price}</p>
 				<div id="todo-list">
-					<p>The busiest people have the most leisure...</p>
+					<p onClick={this.clicked}>The busiest people have the most leisure...</p>
 					<p>{this.state.age}</p>
 					<ul>
 						<li>{this.state.todos[0]}</li>
@@ -44,8 +44,23 @@ var TodoComponent = React.createClass({
 					<ul>{todos}</ul>
 				</div>
 			</div>
-		);
+		); 
+	}, // render
+
+	// Custom functions
+	clicked: function(){
+		console.log('you clicked me!');
+	},
+
+	onDelete: function(item){
+		var updatedTodos = this.state.todos.filter(function(val, index){
+			return item !== val;
+		});
+		this.setState({
+			todos: updatedTodos
+		});
 	}
+
 });
 
 // Create TodoItem component
@@ -55,9 +70,15 @@ var TodoItem = React.createClass({
 			<li>
 				<div className="todo-item">
 					<span className="item-name">{this.props.item}</span>
+					<span className="item-delete" onClick={this.handleDelete}> X </span>
 				</div>
 			</li>
-		)
+		); 
+	},// render
+
+	// Custom functions
+	handleDelete: function(){
+		this.props.onDelete(this.props.item);
 	}
 });
 
